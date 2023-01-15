47ecf6746e2b4aa0f803127bbc0edc50680c7247
Rvega99 <112989004+Rvega99@users.noreply.github.com>
2023-01-15 17:54:25 -0600

    Add files via upload

--- UART.c
+++ UART.c
@@ -1,32 +1,32 @@
 #include "lib/include.h"
 
-extern void Configurar_UART0(void)
+extern void Configurar_UART3(void)
 {
-    SYSCTL->RCGCUART  = (1<<0);   //Paso 1 (RCGCUART) pag.344 UART/modulo0 0->Disable 1->Enable
-    SYSCTL->RCGCGPIO |= (1<<0);     //Paso 2 (RCGCGPIO) pag.340 Enable clock port A
-    //(GPIOAFSEL) pag.671 Enable alternate function
-    GPIOA->AFSEL = (1<<1) | (1<<0);
-    //GPIO Port Control (GPIOPCTL) PA0-> U0Rx PA1-> U0Tx pag.688
-    GPIOA->PCTL = (GPIOA->PCTL&0xFFFFFF00) | 0x00000011;// (1<<0) | (1<<4);//0x00000011
+    SYSCTL->RCGCUART  = (1<<3);   //Paso 1 (RCGCUART) pag.388 UART/modulo0 0->Disable 1->Enable
+    SYSCTL->RCGCGPIO |= (1<<0);     //Paso 2 (RCGCGPIO) pag.382 Enable clock port A
+    //(GPIOAFSEL) pag.1770 Enable alternate function
+    GPIOA_AHB->AFSEL = (1<<5) | (1<<4);
+    //GPIO Port Control (GPIOPCTL) PA4-> U3Rx PA5-> U3Tx pag.741
+    GPIOA_AHB->PCTL = (GPIOA_AHB->PCTL&0xFFFFFF00) | 0x00000011;// (1<<0) | (1<<4);//0x00000011
     // GPIO Digital Enable (GPIODEN) pag.682
-    GPIOA->DEN = (1<<0) | (1<<1);//PA1 PA0
-    //UART0 UART Control (UARTCTL) pag.918 DISABLE!!
-    UART0->CTL = (0<<9) | (0<<8) | (0<<0);
+    GPIOA_AHB->DEN = (1<<0) | (1<<1);//PA4 PA5
+    //UART0 UART Control (UARTCTL) pag.918 Apagar para configurar primero todo y luego encender hasta abajo 
+    UART3->CTL = (0<<9) | (0<<8) | (0<<0);
 
     // UART Integer Baud-Rate Divisor (UARTIBRD) pag.914
-    /*
-    BRD = 25,000,000 / (16*57600) = 27.1267
-    UARTFBRD[DIVFRAC] = integer(.1267 * 64 + 0.5)
-    */
-    UART0->IBRD = 27;
+   //  Configurar_UART3(); //Jesus,Yesica,Carlos,Dulce,Rodolfo,Leonardo -fclk 80MHZ Baud-rate 19200
+        //  BRD = 80,000,000 / (16*19200) =260.4166
+        //  UARTFBRD[DIVFRAC] = integer(.4166 * 64 + 0.5)= 27
+
+    UART3->IBRD = 260;
     // UART Fractional Baud-Rate Divisor (UARTFBRD) pag.915
-    UART0->FBRD = 7;
+    UART3->FBRD = 27;
     //  UART Line Control (UARTLCRH) pag.916
-    UART0->LCRH = (0x3<<5)|(1<<4);
+    UART3->LCRH = (0x3<<5)|(1<<4);
     //  UART Clock Configuration(UARTCC) pag.939
-    UART0->CC =(0<<0);
-    //Disable UART0 UART Control (UARTCTL) pag.918
-    UART0->CTL = (1<<0) | (1<<8) | (1<<9);
+    UART3->CC =(0<<0);
+    //Disable UART3 UART Control (UARTCTL) pag.918
+    UART3->CTL = (1<<0) | (1<<8) | (1<<9);
 
 
 
@@ -38,15 +38,15 @@ extern char readChar(void)
     //UART DR data 906
     int v;
     char c;
-    while((UART0->FR & (1<<4)) != 0 );
-    v = UART0->DR & 0xFF;
+    while((UART3->FR & (1<<4)) != 0 );
+    v = UART3->DR & 0xFF;
     c = v;
     return c;
 }
 extern void printChar(char c)
 {
-    while((UART0->FR & (1<<5)) != 0 );
-    UART0->DR = c;
+    while((UART3->FR & (1<<5)) != 0 );
+    UART3->DR = c;
 }
 extern void printString(char* string)
 {
@@ -56,30 +56,22 @@ extern void printString(char* string)
     }
 }
 
-extern char * readString(char delimitador)
+extern int readString(char delimitador,char *string)
 {
 
    int i=0;
-   char *string = (char *)calloc(10,sizeof(char));
    char c = readChar();
    while(c != delimitador)
    {
        *(string+i) = c;
        i++;
-       if(i%10==0)
-       {
-           string = realloc(string,(i+10)*sizeof(char));
-       }
        c = readChar();
    }
 
-   return string;
+   return i;
 
 }
 //Experimento 2
-
 //El envio es su nombre  (rave) 
-
 // invertirlo y regresarlo con numeros consecutivos
 // entre letras (e1v2a3r) 
-
