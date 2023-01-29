821a7b49d686929a987d1477c2e98e2ad06f61c2
Rvega99 <112989004+Rvega99@users.noreply.github.com>
2023-01-29 13:42:32 -0600

    Ya quedo wuu}

--- UART.c
+++ UART.c
@@ -1,32 +1,50 @@
-#include "lib/include.h"
+ #include "lib/include.h"
 
 extern void Configurar_UART3(void)
 {
-    SYSCTL->RCGCUART  = (1<<3);   //Paso 1 (RCGCUART) pag.388 UART/modulo0 0->Disable 1->Enable
-    SYSCTL->RCGCGPIO |= (1<<0);     //Paso 2 (RCGCGPIO) pag.382 Enable clock port A
-    //(GPIOAFSEL) pag.1770 Enable alternate function
-    GPIOA_AHB->AFSEL = (1<<5) | (1<<4);
-    //GPIO Port Control (GPIOPCTL) PA4-> U3Rx PA5-> U3Tx pag.741
-    GPIOA_AHB->PCTL = (GPIOA_AHB->PCTL&0xFFFFFF00) | 0x00000011;// (1<<0) | (1<<4);//0x00000011
-    // GPIO Digital Enable (GPIODEN) pag.682
-    GPIOA_AHB->DEN = (1<<0) | (1<<1);//PA4 PA5
+    SYSCTL->RCGCUART  = (1<<3); //El registro RCGCUART proporciona al software la capacidad de habilitar
+                                //y deshabilitar los módulos UART en modo Ejecutar UART 3 0->Apagado 1->Encendido
+    
+    SYSCTL->RCGCGPIO |= (1<<0);      //El registro RCGCGPIO proporciona al software la capacidad
+                                     // de habilitar y deshabilitar módulos GPIO en modo de ejecución
+    
+    GPIOA_AHB->AFSEL = (1<<5) | (1<<4);     //(GPIOAFSEL) Habilitar funcion alterna 
+
+    //El registro GPIOPCTL se utiliza junto con el registro GPIOAFSEL y selecciona el registro específico
+    //señal periférica para cada pin GPIO cuando se utiliza el modo de función alternativa
+    GPIOA_AHB->PCTL = (GPIOA_AHB->PCTL&0xFF00FFFF) | 0x00110000;// (1<<0) | (1<<4);//0x00000011
+
+    // El registro GPIODEN es el registro de habilitación digital
+    GPIOA_AHB->DEN = (1<<4) | (1<<5);//PA4 PA5
+
     //UART0 UART Control (UARTCTL) pag.918 Apagar para configurar primero todo y luego encender hasta abajo 
-    UART3->CTL = (0<<9) | (0<<8) | (0<<0);
+    //El registro UARTCTL es el registro de control. Todos los bits se borran al restablecer,
+    //excepto los bits Transmit Enable (TXE) y Receive Enable (RXE),
+    UART3->CTL = (0<<9) | (0<<8) | (0<<0)|(0<<4);
 
     // UART Integer Baud-Rate Divisor (UARTIBRD) pag.914
    //  Configurar_UART3(); //Jesus,Yesica,Carlos,Dulce,Rodolfo,Leonardo -fclk 80MHZ Baud-rate 19200
         //  BRD = 80,000,000 / (16*19200) =260.4166
         //  UARTFBRD[DIVFRAC] = integer(.4166 * 64 + 0.5)= 27
 
-    UART3->IBRD = 260;
-    // UART Fractional Baud-Rate Divisor (UARTFBRD) pag.915
-    UART3->FBRD = 27;
-    //  UART Line Control (UARTLCRH) pag.916
+    //El registro UARTIBRD es la parte entera del valor divisor de velocidad en baudios. 
+    //Todos los bits se borran al reiniciar.
+    UART3->IBRD = 130;
+
+    // Al cambiar el registro UARTFBRD, el nuevo valor no surte efecto hasta
+    //La transmisión/recepción del personaje actual se ha completado
+    UART3->FBRD = 14;
+
+    //  El registro UARTLCRH es el registro de control de línea. 
+    //Parámetros de serie como longitud de datos, paridad,  
+    //y la selección de bits de parada se implementan en este registro.
     UART3->LCRH = (0x3<<5)|(1<<4);
-    //  UART Clock Configuration(UARTCC) pag.939
+
+    //  El registro UARTCC controla la fuente del reloj en baudios para el módulo UART
     UART3->CC =(0<<0);
-    //Disable UART3 UART Control (UARTCTL) pag.918
-    UART3->CTL = (1<<0) | (1<<8) | (1<<9);
+    
+    //Encender UART Control (UARTCTL) 
+    UART3->CTL = (1<<0) | (1<<8) | (1<<9)|(1<<4);
 
 
 }
@@ -37,6 +55,7 @@ extern char readChar(void)
     //UART DR data 906
     int v;
     char c;
+
     while((UART3->FR & (1<<4)) != 0 );
     v = UART3->DR & 0xFF;
     c = v;
